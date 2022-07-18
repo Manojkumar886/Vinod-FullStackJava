c8a4b604a0913a88062730d6381c6e16aceee865
Manojkumar886 <manojgeetha12.10.2001@gmail.com>
2022-07-18 13:06:37 +0530

    DataTypes

---
+++ Vinodkumar-Corejava/src/main/java/Java/Vinodkumar_Corejava/DemoDataTypes.java
@@ -0,0 +1,24 @@
+package Java.Vinodkumar_Corejava;
+
+public class DemoDataTypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;//Print Hole numbers
+		float b=9.8f;//Pointers values(must use f-keyword in end of the value
+		short c=7800;//print only four digit values
+		long mobno=9789355930l;//print more the 9 digit value
+		String name="VinodKumar";//Surrounded by double quotes("") using alphabetic,numeric,Special Character;
+		double salary=9.8;//using pointer values but hole number in use automatically debated double values
+		Boolean InActive=true;//checking concept(true or false)
+		char clas='A';//Surrounded by single quotes('')used single characters
+		System.out.println(name);
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);System.out.println(salary);
+		System.out.println(InActive);
+		System.out.println(clas);
+
+	}
+
+}
